495c472afc0a5c8f4c962aeefe391dda9a3a21ed
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-03-15 01:52:49 -0400

    complete

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 400 && someValue < 2000) {
+    return "That will be twenty bucks.";
+  } else if (someValue > 2500) {
+    return "No can do.";
+  } else if (someValue > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city != "NYC" ? "No go." : "Ok, sounds good.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipValue) {
   // Write your code here!
+  switch (tipValue) {
+    case "generous":
+      return "Thank you so much.";
+
+    case "not as generous":
+      return "Thank you.";
+
+    default:
+      return "Bye.";
+  }
 }
